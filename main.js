4d02ecf8bc8c106a8478e84f71283519d09ef3d6
Contrigra <gaponov60b@gmail.com>
2023-02-27 14:41:51 +0300

    ui design: done

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * (2 + 1))]
 }
 
+function getPlayerChoice(playerSelection) {
+    return playerSelection.className
+}
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    playerSelection = getPlayerChoice(playerSelection)
+    let computerSelection = getComputerChoice()
+
     if (playerSelection === computerSelection) {
         return 'It\'s a draw!'
     }
-
     switch (playerSelection) {
         case 'rock':
             if (computerSelection === 'paper') {
@@ -31,16 +36,10 @@ function playRound(playerSelection, computerSelection) {
             } else {
                 return 'You Win! Scissors beat paper'
             }
-
     }
 }
 
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(prompt().toLowerCase(), getComputerChoice()))
-    }
-
-}
-
-game()
+// TODO counting: add var variables to switch case?
+// TODO winner declaration: if player.winCounter == declareWinner(player)
+let buttons = document.querySelectorAll('button')
+buttons.forEach(e => e.addEventListener('click', () => console.log(playRound(e))));
