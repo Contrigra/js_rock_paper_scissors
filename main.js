7d8b944a135c40f2ee4b9e99f4eba646ac4a19ee
Contrigra <gaponov60b@gmail.com>
2023-02-28 12:47:19 +0300

    project: done

--- main.js
+++ main.js
@@ -10,36 +10,81 @@ function getPlayerChoice(playerSelection) {
     return playerSelection.className
 }
 
-function playRound(playerSelection) {
-    playerSelection = getPlayerChoice(playerSelection)
+function playRound() {
+    let playerSelection = getPlayerChoice(this)
     let computerSelection = getComputerChoice()
+    let resultContainer = document.querySelector('.winner-result')
 
     if (playerSelection === computerSelection) {
-        return 'It\'s a draw!'
-    }
-    switch (playerSelection) {
-        case 'rock':
-            if (computerSelection === 'paper') {
-                return `You Lose! Paper beats rock`
-            } else {
-                return 'You Win! Rock beats scissors'
-            }
-        case 'paper':
-            if (computerSelection === 'scissors') {
-                return 'You Lose! Scissors beat paper'
-            } else {
-                return 'You Win! Paper beats rock'
-            }
-        case 'scissors':
-            if (computerSelection === 'rock') {
-                return 'You Lose! Rock beats scissors'
-            } else {
-                return 'You Win! Scissors beat paper'
-            }
+        resultContainer.textContent = 'It is a draw!';
+    } else {
+        switch (playerSelection) {
+            case 'rock':
+                if (computerSelection === 'paper') {
+                    computerCounter += 1
+                    resultContainer.textContent = '`You Lose! Paper beats rock`'
+                } else {
+                    humanCounter += 1
+                    resultContainer.textContent = 'You Win! Rock beats scissors'
+                }
+                break;
+            case 'paper':
+                if (computerSelection === 'scissors') {
+                    computerCounter += 1
+                    resultContainer.textContent = 'You Lose! Scissors beat paper'
+                } else {
+                    humanCounter += 1
+                    resultContainer.textContent = 'You Win! Paper beats rock'
+                }
+                break;
+            case 'scissors':
+                if (computerSelection === 'rock') {
+                    computerCounter += 1
+                    resultContainer.textContent = 'You Lose! Rock beats scissors'
+                } else {
+                    humanCounter += 1
+                    resultContainer.textContent = 'You Win! Scissors beat paper'
+                }
+                break;
+        }
     }
 }
 
-// TODO counting: add var variables to switch case?
+var humanCounter = 0
+var computerCounter = 0
+
+function updateDivCounter() {
+    const humanNumber = document.querySelector('.player-victories')
+    const computerNumber = document.querySelector('.computer-victories')
+
+    humanNumber.textContent = humanCounter
+    computerNumber.textContent = computerCounter
+}
+
+function checkWinner() {
+
+    let e = document.querySelector('.winner-result')
+
+    if (humanCounter >= 5) {
+        updateDivCounter()
+        e.textContent = 'You Won!'
+        e.classList.add('rainbow')
+    } else if (computerCounter >= 5) {
+        updateDivCounter()
+        e.textContent = 'AI Won!'
+        e.classList.add('rainbow')
+    }
+}
+
+
 // TODO winner declaration: if player.winCounter == declareWinner(player)
-let buttons = document.querySelectorAll('button')
-buttons.forEach(e => e.addEventListener('click', () => console.log(playRound(e))));
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(e => e.addEventListener('click', playRound));
+
+buttons.forEach(e => e.addEventListener('click', () => {
+    (humanCounter < 5 && computerCounter < 5) ? updateDivCounter() : checkWinner()
+}));
+
+// buttons.forEach(e => e.addEventListener('click', updateDivCounter));
+// buttons.forEach(e => e.addEventListener('click', checkWinner));
